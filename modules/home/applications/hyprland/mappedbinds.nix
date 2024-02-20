a1427c02475c4c5c70ef57f6e0e89406efe877d5
lactose <15185244+juuyokka@users.noreply.github.com>
2024-02-20 03:18:16 +0000

    initial work for mapped binds

---
+++ modules/home/applications/hyprland/mappedbinds.nix
@@ -0,0 +1,57 @@
+{ config
+, lib
+, pkgs
+, inputs
+, ...
+}:
+let
+  inherit (lib)
+    mapAttrsToList
+    mkIf
+    mkOption
+    types
+    ;
+  inherit (builtins)
+    toString
+    ;
+
+  mappedBindSubmodule = types.submodule {
+    options = {
+      bindMap = {
+        type = with types; attrsOf str;
+      };
+      binds = {
+        type = with types; attrsOf (funtionTo (functionTo str));
+      };
+    };
+  }
+
+  mapBind = (bindMap: binds:
+    let
+      concatMapAttrsToList = f: a: lib.concatLists (mapAttrsToList f a);
+    in
+    (concatMapAttrsToList
+      (x: y:
+        (mapAttrsToList
+          (_: b: b x y)
+          binds))
+      bindMap));
+
+  cfg = config.kalyx.hyprland;
+in
+{
+  options.kalyx.hyprland = {
+    mappedBinds = {
+      type = types.attrsOf mappedBindSubmodule;
+      default = { };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    wayland.windowManager.hyprland = {
+      settings = {
+        bind = mapAttrsToList (_: v: mapBind v.bindMap v.binds) cfg.mappedBinds;
+      };
+    };
+  };
+}
