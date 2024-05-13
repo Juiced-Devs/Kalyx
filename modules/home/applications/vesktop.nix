941231dc1bdd26460f4ff095d858743d842bd12e
SkettiSouls <skettisouls@gmail.com>
2024-05-13 06:47:29 -0500

    Added vesktop package option.
    Added an option to specify the installed package.
    Replaced the comment about needing an unstable overlay with a version requirement.
    Slightly changed examples.
    Added inherits for builtins.isPath and builtins.isString.

--- modules/home/applications/vesktop.nix
+++ modules/home/applications/vesktop.nix
@@ -3,31 +3,34 @@
 let
   inherit (lib)
     mapAttrs'
-    mkDefault
     mkEnableOption
     mkIf
     mkMerge
     mkOption
+    mkPackageOption
     nameValuePair
     recursiveUpdate
     types
     ;
   inherit (builtins)
+    isPath
+    isString
     listToAttrs
     ;
 
-  cfg = config.kalyx.vesktop;
+  cfg = config.programs.vesktop;
   makeConfig = builtins.toJSON;
 in
 {
-  options.kalyx.vesktop = {
+  options.programs.vesktop = {
     enable = mkEnableOption "Vesktop discord client";
+    package = mkPackageOption pkgs "vesktop" { };
     settings = mkOption {
       description = ''Vencord settings.'';
       default = { };
       type = types.attrs;
-      example = literalExpression ''
-        {
+      example = ''
+        programs.vesktop.settings = {
           notifyAboutUpdates = true;
           autoUpdate = true;
           autoUpdateNotification = true;
@@ -41,15 +44,15 @@ in
           enableReactDevtool = false;
           frameless = false;
           transparent = false;
-        }
+        };
       '';
     };
     state = mkOption {
       description = ''Vesktop settings.'';
       default = { };
       type = types.attrs;
-      example = literalExpression ''
-        {
+      example = ''
+        programs.vesktop.state = {
           discordBranch = "stable";
           firstLaunch = false;
           arRPC = "on";
@@ -58,7 +61,7 @@ in
           minimizeToTray = false;
           splashTheming = true;
           customTitleBar = false;
-        }
+        };
       '';
     };
     plugins = mkOption {
@@ -71,8 +74,9 @@ in
           default = { };
         };
       });
-      example = literalExpression ''
-        {
+      example = ''
+        programs.vesktop.plugins = {
+          GifPaste.enable = true;
           iLoveSpam.enable = false;
           ImageZoom = {
             enable = true;
@@ -86,44 +90,40 @@ in
               zoomSpeed = 0.5;
             };
           };
-        }
+        };
       '';
     };
     enabledPlugins = mkOption {
       description = ''List of plugins to enable.''; # Useful for plugins that don't have settings.
       default = [ ];
       type = with types; listOf str;
-      example = literalExpression ''
-        [
+      example = ''
+        programs.vesktop.enabledPlugins = [
           AlwaysAnimate
           GifPaste
           ImageZoom
-        ]
+        ];
       '';
     };
     notifications = mkOption {
       description = ''Vencord notification settings.'';
       default = { };
       type = with types; attrsOf (either str int);
-      example = literalExpression ''
-        {
-          timeout = 5000;
-          position = "bottom-right";
-          useNative = "not-focused";
-          logLimit = 50;
-        }
+      example = ''
+        timeout = 5000;
+        position = "bottom-right";
+        useNative = "not-focused";
+        logLimit = 50;
       '';
     };
     cloud = mkOption {
       description = ''Vencord cloud integration settings.'';
       default = { };
       type = types.attrs;
-      example = literalExpression ''
-        {
-          authenticated = true;
-          url = "https://api.vencord.dev";
-          settingsSync = true;
-        }
+      example = ''
+        authenticated = true;
+        url = "https://api.vencord.dev";
+        settingsSync = true;
       '';
     };
     themes = mkOption {
@@ -134,31 +134,27 @@ in
           type = with types; (either lines path);
         };
       });
-      example = literalExpression ''
-        {
-          mytheme.css = ./path/to/css;
-          mytheme1.css = '
-            theme line 1
-            theme line 2
-          ';
-        }
+      example = ''
+        programs.vesktop.themes = {
+          mytheme.css = "custom css";
+          mytheme2.css = ./path/to/css;
+          };
+        };
       '';
     };
   };
 
   config = mkIf cfg.enable {
-    # Temporarily requires an overlay for nixpkgs unstable, as Vesktop v1.5.0 rebranded from vencorddesktop, changing file structures and directory names. Will be removed after the first 2024 release of nixpkgs.
-    home.packages = with pkgs; [
-      unstable.vesktop
-    ];
+    # Requires Vesktop v1.5.0 or new, as vesktop was rebranded from vencorddesktop, changing file structures and directory names.
+    home.packages = [ cfg.package ];
 
     home.file = mkMerge ([
       (mapAttrs'
         (name: themes: nameValuePair
           ".config/vesktop/themes/${name}.css"
           {
-            text = mkIf (builtins.isString themes.css) themes.css;
-            source = mkIf (builtins.isPath themes.css) themes.css;
+            text = mkIf (isString themes.css) themes.css;
+            source = mkIf (isPath themes.css) themes.css;
           })
         cfg.themes)
 
