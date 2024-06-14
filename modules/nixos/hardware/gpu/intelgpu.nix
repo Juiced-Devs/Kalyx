e9039e471fc5310b745b8398f14bd10d51ad651b
kyleraykbs <kyleraykbs@proton.me>
2024-06-13 22:59:48 -0700

    removed vaapiIntel as it doesnt build

--- modules/nixos/hardware/gpu/intelgpu.nix
+++ modules/nixos/hardware/gpu/intelgpu.nix
@@ -24,7 +24,6 @@ in
     hardware.opengl = {
       extraPackages = with pkgs; [
         intel-media-driver # LIBVA_DRIVER_NAME=iHD
-        vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
         vaapiVdpau
         libvdpau-va-gl
       ];
